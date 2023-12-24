cc38715b925c9c6c0bb5a93b42be24fea55e44a7
adl <makara3416@gmail.com>
2023-12-24 23:17:29 +0100

    v

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import City from './city';
 function App() {
   const key ="aeb6cab69d44bac1f55124b25acb4e10";
 
-  const [city, setCity] = useState(); 
   const [search, setSearch] = useState(""); 
+  const [city, setCity] = useState(); 
+ 
 
 
 
@@ -23,6 +24,7 @@ function App() {
         `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
         );
       console.log(response);
+      setCity(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -38,12 +40,15 @@ function App() {
   return (
   <div className="App">
     <div>
-      <input 
-      onChange={(e) => setSearch(e.target.value)} 
-      type="text"
-      placeholder='Placeholder'
-      className='border-8 bg-slate-500' /> 
-      <City city={city}/>
+    <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Hava durumunu istediginiz Sehri giriniz"
+          className=" my-5 px-3 w-[270px] py-3 placeholder-blueGray-300 text-blueGray-600   bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring "
+        />
+
+     { city && < City city={city}/>}
+
     </div>
     </div>
     );
