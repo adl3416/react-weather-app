2e1c4c9614d77e7175161467638b140df6604977
adl <makara3416@gmail.com>
2023-12-24 22:54:56 +0100

    v

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import './App.css';
 import { useEffect, useState } from 'react';
+import City from './city';
 
 
 
 function App() {
   const key ="aeb6cab69d44bac1f55124b25acb4e10";
 
-  const [city, setCity] = useState();
+  const [city, setCity] = useState(); 
+  const [search, setSearch] = useState(""); 
 
 
 
@@ -18,7 +20,7 @@ function App() {
   async function getApi() {
     try {
       const response = await axios.get( 
-        `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
         );
       console.log(response);
     } catch (error) {
@@ -27,13 +29,21 @@ function App() {
   }
 
   getApi();
+
   
- }, []);  // arayn in ici bos ise uygulama ilk calistig zaman render olcak
+ }, [search]);  // arayn in ici bos ise uygulama ilk calistig zaman render olcak
+
+ console.log(search)
  
   return (
   <div className="App">
     <div>
-      <input onChange={(e) => console.log(e.target.value)} className='border-8 bg-slate-500' type='text'/> 
+      <input 
+      onChange={(e) => setSearch(e.target.value)} 
+      type="text"
+      placeholder='Placeholder'
+      className='border-8 bg-slate-500' /> 
+      <City city={city}/>
     </div>
     </div>
     );
---
+++ src/city.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const City = ({city}) => {
+
+     console.log(city)
+  return (
+
+    <div>
+          <div>
+               <h1>{city.main.temp}</h1>
+               <h1>
+                    {city.}
+               </h1>
+          </div>
+    </div>
+  )
+}
+
+export default City
