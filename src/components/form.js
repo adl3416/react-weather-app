1b9643368e6388c0b50e9f4fad8d5ce85c413a58
adl <makara3416@gmail.com>
2024-07-02 08:08:26 +0200

    v

--- src/components/form.js
+++ src/components/form.js
@@ -25,8 +25,7 @@ const Form = ({ info, setInfo, setState }) => {
 
   return (
     <div>
-      <h1> HARUN  Abi </h1>
-      <h1> Erzincan  a  da Bak</h1>
+      
       <h1>Hava Durumu </h1>
 
       <form
